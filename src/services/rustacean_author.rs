ebf3ec21cd43976b9d254b92a05203dd636a9d83
Luciano Ramello <luchosr@gmail.com>
2025-01-07 15:08:41 +0100

    feat(services): improve rustacean member  api call

--- src/services/rustacean_author.rs
+++ src/services/rustacean_author.rs
@@ -6,6 +6,7 @@ use url::Url;
 pub struct RustaceanMember {
     pub name: String,
     pub pull_request_number: String,
+    pub token: String,
 }
 
 #[derive(Deserialize, Debug)]
@@ -14,22 +15,43 @@ pub struct GitHubFile {
 }
 
 impl RustaceanMember {
-    pub fn new(name: String, pull_request_number: String) -> Self {
+    pub fn new(name: String, pull_request_number: String, token: String) -> Self {
         Self {
             name,
             pull_request_number,
+            token,
         }
     }
 
     pub async fn is_rustacean_valid(&self) -> Result<bool> {
         let url = Url::parse(&format!(
-            "https://api.github.com/repos/rustacean-sh/rustacean-authorship-action/pulls/{}/files",
+            "https://api.github.com/repos/rustacean-sh/rustacean.sh/pulls/{}/files",
             self.pull_request_number
         ))?;
 
-        let pr_files = reqwest::get(url).await?.json::<Vec<GitHubFile>>().await?;
+        let client = reqwest::Client::new();
 
-        println!("Files are: {:?}", pr_files[0]);
-        Ok(true)
+        let pr_files = client
+            .get(url)
+            .header("Accept", "application/json")
+            .header("User-Agent", "Rust")
+            .bearer_auth(&self.token)
+            .send()
+            .await?
+            .json::<Vec<GitHubFile>>()
+            .await?;
+
+        if pr_files.len() == 2
+            && pr_files[1].filename == format!("data/rustaceans/{}.toml", self.name)
+        {
+            println!("Files are: {:?}", pr_files[1].filename);
+            Ok(true)
+        } else {
+            println!(
+                "the only file that you need to add is: data/rustaceans/{}.toml",
+                self.name
+            );
+            Ok(false)
+        }
     }
 }
