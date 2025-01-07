4999dc1dcd0b897c79449bbd8eb7a04b37668710
Luciano Ramello <luchosr@gmail.com>
2025-01-07 15:09:16 +0100

    feat: implement rustacean member api call

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,25 @@ async fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     let pr_author = args.get(1).context("PR Author is required")?;
     let pr_number = args.get(2).context("PR Number is required")?;
+    let token = args.get(3).context("Github token is required")?;
 
     if pr_author.is_empty() {
         bail!("PR Author Cannot Be Empty!");
     }
 
-    let rustacean = RustaceanMember::new(pr_author.to_string(), pr_number.to_string());
+    let rustacean = RustaceanMember::new(
+        pr_author.to_string(),
+        pr_number.to_string(),
+        token.to_string(),
+    );
 
     let rustacean_valid =
         services::rustacean_author::RustaceanMember::is_rustacean_valid(&rustacean).await?;
 
-    println!("Hello {pr_author} your PR number is {pr_number}, and you are {rustacean_valid}");
+    println!(
+        "Hello {pr_author} your PR number is {pr_number}, and your changes are {}",
+        if rustacean_valid { "valid" } else { "invalid" }
+    );
 
     Ok(())
 }
